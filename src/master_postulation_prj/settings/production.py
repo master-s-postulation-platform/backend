ea1e8bf748e13df4a309ead1d77cad460cbd1740
Wilson Antury <ventas@antury.net>
2021-06-30 12:14:37 -0500

    [ADD] statics collect issue

--- src/master_postulation_prj/settings/production.py
+++ src/master_postulation_prj/settings/production.py
@@ -19,6 +19,5 @@ DATABASES = {
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
